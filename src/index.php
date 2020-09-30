9858c1b48269ddd72b00d04e5f35f1f030079d4c
Chenuet Loic <loicchenuet@hotmail.fr>
2020-09-30 11:38:24 +0200

    ajout de la barre nav

--- src/index.php
+++ src/index.php
@@ -8,9 +8,17 @@
     <title>Perceval le Gallois</title>
     <meta name="description" content="Le CV en ligne de Provençal le Gaulois, aspirant chevalier de la Table Ronde."/>
     <meta name="author" content="Les Petits Pédestres"/>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-
+<nav class="nav" >
+    <img src="https://i.postimg.cc/wTbLw5dz/Webp-net-resizeimage.png" alt="logoKaamelott" width="100px" height="100px">
+    <a class="aboutUs" href=#aboutUs>About us</a>
+    <a class="skill" href=#skill>Skill</a>
+    <a class="productions" href=#productions>Productions</a>
+    <a class="experience" href=#experience>Experiences</a>
+    <a class="formation" href=#formation>Formations</a>
+    <a class="contactUs" href=#contactUs>Contact us</a>
+</nav>
 </body>
 </html>
