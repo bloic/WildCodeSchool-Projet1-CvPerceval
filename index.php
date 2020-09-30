a33791f4c7fea708fddd6b8888de82f9607f4bc9
Chenuet Loic <loicchenuet@hotmail.fr>
2020-09-30 12:00:16 +0200

    mise en place de l'index hors du dossier

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Perceval le Gallois</title>
+    <meta name="description" content="Le CV en ligne de Provençal le Gaulois, aspirant chevalier de la Table Ronde."/>
+    <meta name="author" content="Les Petits Pédestres"/>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<nav class="nav" >
+    <img src="https://i.postimg.cc/wTbLw5dz/Webp-net-resizeimage.png" alt="logoKaamelott" width="100px" height="100px">
+    <a class="aboutUs" href=#aboutUs>About us</a>
+    <a class="skill" href=#skill>Skill</a>
+    <a class="productions" href=#productions>Productions</a>
+    <a class="experience" href=#experience>Experiences</a>
+    <a class="formation" href=#formation>Formations</a>
+    <a class="contactUs" href=#contactUs>Contact us</a>
+</nav>
+</body>
+</html>
