d106e549cbfb769e627ff54485b5dd6d84526dbf
bloic <loic.benos@gmail.com>
2020-09-30 12:29:25 +0200

    I add the tag p

--- src/Experience.php
+++ src/Experience.php
@@ -1,10 +1,10 @@
 <section id="#experience">
     <h2 >Expériences</h2>
 
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
     Aut cum iste saepe totam voluptas. Corporis, dolor dolorum ea esse eum
     ex harum reiciendis reprehenderit rerum soluta totam velit vitae voluptates.
-    ex harum reiciendis reprehenderit rerum soluta totam velit vitae voluptates.
+    ex harum reiciendis reprehenderit rerum soluta totam velit vitae voluptates.</p>
 
 </section>
 <?php
--- src/Formation.php
+++ src/Formation.php
@@ -1,10 +1,10 @@
 <section id="#formation">
 <h2>Formations</h2>
 
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
     Aut cum iste saepe totam voluptas. Corporis, dolor dolorum ea esse eum
     ex harum reiciendis reprehenderit rerum soluta totam velit vitae voluptates.
-    ex harum reiciendis reprehenderit rerum soluta totam velit vitae voluptates.
+    ex harum reiciendis reprehenderit rerum soluta totam velit vitae voluptates.</p>
 
 </section>
 <?php
