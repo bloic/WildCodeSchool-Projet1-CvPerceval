2139895fe8b72c4927e781bb88cdab2d018d0811
Chenuet Loic <loicchenuet@hotmail.fr>
2020-09-30 21:37:50 +0200

    .

--- index.php
+++ index.php
@@ -5,30 +5,38 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Eagle+Lake&family=Roboto&display=swap" rel="stylesheet">
     <title>Perceval le Gallois</title>
     <meta name="description" content="Le CV en ligne de Provençal le Gaulois, aspirant chevalier de la Table Ronde."/>
     <meta name="author" content="Les Petits Pédestres"/>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<nav class="nav" >
-    <img src="https://i.postimg.cc/wTbLw5dz/Webp-net-resizeimage.png" alt="logoKaamelott" width="100px" height="100px">
-    <a class="aboutUs" href=#aboutUs>About us</a>
-    <a class="skill" href=#skill>Skill</a>
-    <a class="productions" href=#productions>Productions</a>
-    <a class="experience" href=#experience>Experiences</a>
-    <a class="formation" href=#formation>Formations</a>
-    <a class="contactUs" href=#contactUs>Contact us</a>
-</nav>
-<main>
-    <section>
-        <h2>À propos</h2>
-        <img src="https://via.placeholder.com/200x200" alt="Photo Perceval"/>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur culpa illo illum nesciunt. Aspernatur
-            id, nostrum quae sequi temporibus voluptate! Distinctio dolores facilis hic laboriosam modi nulla rem
-            repudiandae vel.</p>
-    </section>
-</main>
+
+<nav class="nav">
+    <div>
+        <img class='logo' src="https://i.postimg.cc/wTbLw5dz/Webp-net-resizeimage.png" alt="logoKaamelott" width="100px"
+             height="100px">
+    </div>
+
+    <div class="menu-items" id="menu">
+        <a class="aboutUs" href=#aboutUs>About us</a>
+        <a class="skill" href=#skill>Skill</a>
+        <a class="productions" href=#productions>Productions</a>
+        <a class="experience" href=#experience>Experiences</a>
+        <a class="formation" href=#formation>Formations</a>
+        <a class="contactUs" href=#contactUs>Contact us</a>
+    </div>
+
+    <div class="iconFennel">
+        <a href="#menu"><img src="https://i.postimg.cc/5NkDq0Ys/fennel.png" alt="logofennel" width="50px"
+                                          height="50px"></a>
+    </div>
+
+
+</nav>
+
+
 </body>
 </html>
