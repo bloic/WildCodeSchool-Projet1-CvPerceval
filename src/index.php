c69d75b2ba0e3cb6c7431930add12bcbe91c8ec5
Flichyn <nicolasflichy@gmail.com>
2020-09-30 09:37:59 +0200

    Add HTML skeleton

--- src/index.php
+++ src/index.php
@@ -5,11 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Perceval le Gallois</title>
+    <meta name="description" content="Le CV en ligne de Provençal le Gaulois, aspirant chevalier de la Table Ronde."/>
+    <meta name="author" content="Les Petits Pédestres"/>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <?php
 
-    ?>
 </body>
 </html>
