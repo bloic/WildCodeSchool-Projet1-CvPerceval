0cd0c0b192fbc434d6daffc90b9587a11d1c68fa
Chenuet Loic <loicchenuet@hotmail.fr>
2020-10-01 18:21:12 +0200

    ajout barre nav

--- index.php
+++ index.php
@@ -14,32 +14,7 @@
 <body>
 
 
-<nav class="nav">
-
-    <div class="divLogo">
-        <img class="logoKaamelott" src="img/logoKaamelott.png" alt="logoKaamelott">
-    </div>
-
-    <div class="backgroundMenu">
-        <div class="menu-items" id="menu">
-            <div class="bouton"><a href=#aboutUs>About us</a></div>
-            <div class="bouton"><a href=#skill>Skill</a></div>
-            <div class="bouton"><a href=#productions>Productions</a></div>
-            <div class="bouton"><a href=#experience>Experiences</a></div>
-            <div class="bouton"><a href=#formation>Formations</a></div>
-            <div class="bouton"><a href=#contactUs>Contact us</a></div>
-        </div>
-    </div>
-
-    <div class="iconFennel">
-        <a href="#menu"><img src="img/logoFennel.png" alt="logofennel"
-                             width="50px"
-                             height="50px"></a>
-        <div class="close"><a href=#menu>X</a></div>
-    </div>
-
-
-</nav>
+<?php include 'barNav.php'; ?>
 
 
 </body>
