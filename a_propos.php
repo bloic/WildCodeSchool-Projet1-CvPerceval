3337099ae76b1ffa5a9464e74e97591e5ac45977
Flichyn <nicolasflichy@gmail.com>
2020-10-01 13:39:31 +0200

    Changes on CSS

--- a_propos.php
+++ a_propos.php
@@ -1,5 +1,13 @@
-<h2>À propos</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam aspernatur id inventore laborum, magnam
-    nemo nihil odio, optio quas quia recusandae ullam voluptate? Aperiam beatae cum delectus explicabo similique.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium aperiam commodi deserunt eveniet, incidunt,
-    inventore iste itaque laboriosam minus nihil optio qui quibusdam quo rem repellat sapiente sint voluptatem.</p>
+<h2 id="apropos-h2">À propos</h2>
+<div class="apropos-flex">
+    <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam aspernatur id inventore laborum,
+            magnam
+            nemo nihil odio, optio quas quia recusandae ullam voluptate? Aperiam beatae cum delectus explicabo
+            similique.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium aperiam commodi deserunt eveniet,
+            incidunt,
+            inventore iste itaque laboriosam minus nihil optio qui quibusdam quo rem repellat sapiente sint
+            voluptatem.</p>
+    </div>
+</div>
