0aee1c621b0a7d26b8613ac562a547ae087fdf00
Flichyn <nicolasflichy@gmail.com>
2020-10-01 16:56:56 +0200

    Add some CSS

--- a_propos.php
+++ a_propos.php
@@ -1,13 +1,7 @@
 <h2 id="apropos-h2">À propos</h2>
 <div class="apropos-flex">
-    <div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam aspernatur id inventore laborum,
-            magnam
-            nemo nihil odio, optio quas quia recusandae ullam voluptate? Aperiam beatae cum delectus explicabo
-            similique.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium aperiam commodi deserunt eveniet,
-            incidunt,
-            inventore iste itaque laboriosam minus nihil optio qui quibusdam quo rem repellat sapiente sint
-            voluptatem.</p>
-    </div>
+    <p>Ancien paysan du Pays de Galles à Caerdydd ayant suivi plusieurs formations pluridisciplinaires dans le
+        divertissement (Sloubi, Cul de Chouette (avec les règles à l'Aquitaine), le Sirop), j'aimerais aujourd'hui <strong>qu'on me considère en tant que tel</strong>. Fort de mes compétences en tant
+        qu'<strong>écuyer des chevaliers de Tintagel</strong>, je souhaite maintenant aller plus loin, et devenir chevalier de la Table
+        Ronde au service du Roi Arthur Pendragon. Parce que, oui, <strong>on en a gros, Sire.</strong></p>
 </div>
