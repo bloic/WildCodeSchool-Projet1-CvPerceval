0fb723277cd7cf75decf3aa6523b26b60a29e03e
bloic <loic.benos@gmail.com>
2020-10-01 21:40:19 +0200

    I add the content to the training and experience section. I've been styled on the css

--- src/Experience.php
+++ src/Experience.php
@@ -1,14 +1,30 @@
 <section class="experience" id="experience">
     <h2 class="title">Expériences</h2>
 
-    <p class="text-light">
-        <ul>
-            <li>Quêtes des deux Renards</li>
-            <li>Quêtes du Serpent Géant</li>
-            <li>Quêtes du Graal</li>
-            <li>Le C'EST PAS FAUX</li>
-        </ul>
-    </p>
+    <div class="containers">
+        <div class="date">
+            <div class="row"></div>
+                <div class="dateQuests">1400ans avant Jscript</div>
+        </div>
+        <div class="cadre">
+            <div class="quests"><h3>Université de Karadoc</h3>
+                <p>Quêtes des deux Renards</p></div>
+        </div>
+    </div>
+
+    <br>
+    <div class="containers">
+        <div class="date">
+            <div class="row"></div>
+                <div class="dateQuests">1400ans avant Jscript</div>
+        </div>
+        <div class="cadre">
+            <div class="quests"><h3>Table Ronde</h3>
+                <p>Quêtes du Graal</p>
+            </div>
+        </div>
+    </div>
+
 
 </section>
 <?php
--- src/Formation.php
+++ src/Formation.php
@@ -3,59 +3,59 @@
 
     <div class="containers">
         <div class="date">
-            <div class="historyDate">1400ans avant Jscript</div>
-                <div class="row"></div>
+            <div class="row"></div>
+                <div class="historyDate">1400ans avant Jscript</div>
         </div>
             <div class="cadre">
-                <div class="school"><h3>Université Karadoc</h3></div>
-                    <div class="diploma">Doctorat de La botte Secrète Karadoc "C'est pas Faux"</div>
+                <div class="dateDiploma"><h3>Université de Karadoc</h3>
+                    <p>Doctorat de La botte Secrète Karadoc "C'est pas Faux"</p></div>
             </div>
     </div>
 
     <br>
      <div class="containers">
          <div class="date">
-             <div class="historyDate">1400ans avant Jscript</div>
-                <div class="row"></div>
+             <div class="row"></div>
+                <div class="historyDate">1400ans avant Jscript</div>
+
          </div>
          <div class="cadre">
-             <div class="school"><h3>Université Le Gallois</h3></div>
-                 <div class="diploma">Master de La technique du Rebrouse chemin </div>
+                 <div class="dateDiploma"><h3>Université Le Gallois</h3>
+                     <p>Master de La technique du Rebrouse chemin</p> </div>
          </div>
      </div>
 
 
     <br>
     <div class="containers">
-        <div class="date">
-            <div class="historyDate">1400ans avant Jscript</div>
+            <div class="date">
                 <div class="row"></div>
-        </div>
+                    <div class="historyDate">1400ans avant Jscript</div>
+            </div>
         <div class="cadre">
-            <div class="school"><h3>Stargate</h3></div>
-         <div class="diploma">testeur de Porte dimensionnelle Stargate</div>
+            <div class="dateDiploma"><h3>Stargate</h3>
+                <p>testeur de Porte dimensionnelle Stargate</p></div>
     </div>
 
     <br>
     <div class="containers">
-        <div class="date">
-            <div class="historyDate">1400ans avant Jscript</div>
+            <div class="date">
                 <div class="row"></div>
-        </div>
+                    <div class="historyDate">1400ans avant Jscript</div>
+            </div>
         <div class="cadre">
-            <div class="school"><h3>Université Le Gallois</h3></div>
-         <div class="diploma">Perceval et le contre-sirop</div>
+         <div class="dateDiploma"><h3>Université Le Gallois</h3>
+             <p>Perceval et le contre-sirop</p></div>
      </div>
 
     <br>
     <div class="containers">
         <div class="date">
-            <div class="historyDate">1400ans avant Jscript</div>
-                <div class="row"></div>
+            <div class="row"></div>
+                <div class="historyDate">1400ans avant Jscript</div>
         </div>
         <div class="cadre">
-            <div class="school"><h3>Université Le Gallois</h3></div>
-         <div class="diploma">Perceval chante Sloubi</div>
+         <div class="dateDiploma"><h3>Université Le Gallois</h3><p>Perceval chante Sloubi</p></div>
      </div>
 
 
