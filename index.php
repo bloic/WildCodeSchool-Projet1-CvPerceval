bcd36226a33a782618d03c2942aa6ce36c76ac10
Flichyn <nicolasflichy@gmail.com>
2020-10-01 22:47:11 +0200

    Finished realisations section

--- index.php
+++ index.php
@@ -12,9 +12,7 @@
 </head>
 <body>
 <main>
-    <section class="text-light realisations-background-color">
-        <?php include 'realisations.php'; ?>
-    </section>
+    <?php include 'realisations.php'; ?>
 </main>
 </body>
 </html>
--- realisations.php
+++ realisations.php
@@ -1,18 +1,16 @@
 <section class="text-light realisations-background-color">
     <h2>Réalisations</h2>
     <div class="real-flex">
-        <div class="real-img">
-            <h3 class="real-h3">Champion régional de Sloubi</h3>
-            <p class="real-text">Lorem ipsum dolor sit amet</p>
+        <div class="real-img sloubi-img">
+            <p id="p-sirop">Règles simplifiées du Sirop</p>
         </div>
 
-        <div class="real-img">
-            <h3>Real 2</h3>
-            <p>Lorem ipsum dolor sit amet</p>
+        <div class="real-img unagi-img">
+            <p id="p-unagi">Technique de combat "Unagi"</p>
         </div>
 
-        <div class="real-img">
-            <h3>Real 3</h3>
-            <p>Lorem ipsum dolor sit amet</p>
+        <div class="real-img fishing-img">
+            <p id="p-fishing">Technique de la pêche sans hameçon "pour comprendre des trucs"</p>
         </div>
     </div>
+</section>
