0f5e02e42d3833c8f46117124d723b49b8fc906a
Chenuet Loic <loicchenuet@hotmail.fr>
2020-10-07 22:49:48 +0200

    add recommandation bouton etc...

---
+++ Recommandation.php
@@ -0,0 +1,130 @@
+<section id="recommandations">
+    <div><h2 class="titleRecommandation">Recommandation</h2></div>
+
+
+    <div class=" container">
+
+
+        <div class="arthur">
+            <div>
+                <img src="/img/arthur.jpg" alt="Arthur Roi de Bretagne" class="imageArthur">
+            </div>
+
+            <div class="recommandationArthur">
+
+                <img src="/img/left_quote-30_30.png">
+                <ul>
+                    <li>
+                        <p> Je gueule ; c'est vrai, j'suis un peu sec, tout ça, mais pour quelqu'un comme moi qui a
+                            facilement
+                            tendance à la dépression c'est très
+                            important ce que vous faites, parce que... Comment vous dire... C'est systématiquement
+                            débile mais
+                            c'est toujours inattendu. Et ça c'est très important pour la... la santé du... du
+                            cigare...</p>
+                    </li>
+                    <li>
+                        <p> Nouvelle technique : on passe pour des cons, les autres se marrent, et on frappe.
+                            C’est nouveau. […] Ah non, ça c’est que nous. Parce qu’il faut être capable de passer pour
+                            des cons en un temps record. Ah non, là-dessus on a une avance considérable. <img
+                                    src="/img/right_quote_30_30.png"></p>
+                    </li>
+                </ul>
+
+
+            </div>
+        </div>
+
+
+        <div class="karadoc">
+            <div class="recommandationKaradoc">
+                <img src="/img/left_quote-30_30.png">
+                <ul>
+                    <li>
+                        <p>La joie de vivre et le jambon, y'a pas trente-six recettes du bonheur !</p>
+                    </li>
+                    <li>
+                        <p>Là, y'a les méduses, les insectes. Là, y'a les glandus, les grouillots. Là, y'a les mecs
+                            normaux.
+                            Là, y'a les chevaliers. Là, y'a les rois
+                            et les princes. Et après, bien au-dessus, y'a le Roi Arthur. Vous, vous aurez eu deux
+                            bonhommes dans
+                            votre vie, eh ben vous pourrez dire que vous avez tapé dans l'exception. <img
+                                    src="/img/right_quote_30_30.png"></p>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <img src="/img/Karadoc-150x150.jpg" alt="Karadoc" class="imageKaradoc">
+            </div>
+        </div>
+
+
+        <div class="maitreArme">
+            <div>
+                <img src="/img/LeMaîtredArmes-150x150.jpg" alt="Le Maitre d'armes" class="imageMaitreArme">
+            </div>
+            <div class="recommandationMaitreArme">
+                <img src="/img/left_quote-30_30.png">
+                <ul>
+                    <li>
+                        <p>Euh, juste une chose... Manquez
+                            encore une seule fois de respect au futur roi de Bretagne, et je vous coupe les boules ! Ca
+                            vous
+                            fera une jolie petite sacoche pour ranger vos dés à coudre.</p>
+                    </li>
+                    <li>
+                        <p>Vous savez quoi, Sire ? On va commencer par se faire une saucisse grillée de trois pieds de
+                            long, avec un tonnelet de pinard chacun, et derrière, peut être bien qu'on se paiera des
+                            filles.
+                            Ah oui ! A un moment, vive la vie !<img src="/img/right_quote_30_30.png"></p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="angharade">
+            <div class="recommandationAngharade">
+                <img src="/img/left_quote-30_30.png">
+                <ul>
+                    <li>
+                        <p> Monsieur peut considérer que je fais partie d'une sorte de blot. Je viens en sus.</p>
+                    </li>
+                    <li>
+                        <p>Je dois avouer que la chose a du bon, si c'est pour m'éviter la vue de la grande décarade,
+                            le défilé des nouvelles têtes, la parade des amoureuses à calcul, les poids lourds de la
+                            jambe légère, le festival de la morue, en somme... <img src="/img/right_quote_30_30.png">
+                        </p>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <img src="/img/Angharad-150x150.jpg" alt="Angharade" class="imageAngharade">
+            </div>
+        </div>
+
+
+        <div class="leodagan">
+            <div>
+                <img src="/img/Léodagan-150x150.jpg" alt="Léodagan" class="imageLeodagan">
+            </div>
+
+            <div class="recommandationLeodagan">
+                <img src="/img/left_quote-30_30.png">
+                <ul>
+                    <li>
+                        <p> Personnellement, la condition paysanne, j'me la taille en biseau, voyez.</p>
+                    </li>
+                    <li>
+                        <p> Ooooooooh, non mais faire du labyrinthe avec un trou-de-balle pareil, faut drôlement de la
+                            vaillance, hein ! <img src="/img/right_quote_30_30.png"></p>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+
+
+</section>
