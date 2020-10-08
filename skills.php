f6461122ce40b4b4c20f590a28d4a220f7e241b1
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2020-10-08 19:14:34 +0200

    add section padding and php

--- skills.php
+++ skills.php
@@ -2,6 +2,45 @@
     <h2 class="skillsMainTitle">Compétences</h2>
 
     <div class="skillContainerGlobal">
+        <?php
+
+        $skills = [
+            ['title' => 'Combat',
+                'titleClass' => 'skillsTitle',
+                'iconClass' => 'skillIconSwords',
+                'text' => 'Expertise dans le maniement d\'objets redondants',
+                'textClass' => 'skillsText',
+            ],
+            ['title' => 'Stratégie',
+                'titleClass' => 'skillsTitle',
+                'iconClass' => 'skillIconCastle',
+                'text' => 'Comptage des briques au centième près de manière péremptoire',
+                'textClass' => 'skillsText',
+            ],
+            ['title' => 'Communication',
+                'titleClass' => 'skillsTitle',
+                'iconClass' => 'skillIconOld',
+                'text' => 'Spécialisé dans la gestion des vieux mystérieux',
+                'textClass' => 'skillsText',
+            ],
+        ];
+
+        foreach ($skills as $skill) {
+        ?>
+        <div class="skillContainer">
+            <div class="<?=$skill['iconClass'];?>"></div>
+            <h3 class="<?=$skill['titleClass'];?>"><?=$skill['title'];?></h3>
+            <p class="<?=$skill['textClass'];?>"><?=$skill['text'];?></p>
+        </div>
+        <?php
+}
+        ?>
+
+    </div>
+</section>
+
+
+
 
         <!--<div class="skillContainer">
             <div class="skillIconSwords"></div>
@@ -20,38 +59,3 @@
             <h3 class="skillsTitle">Communication </h3>
             <p class="skillsText">Spécialisé dans la gestion des vieux mystérieux</p>
         </div>-->
-    </div>
-</section>
-
-<?php
-
-$skills = [
-    ['title' => 'Combat',
-        'titleClass' => 'skillsTitle',
-        'iconClass' => 'skillIconSwords',
-        'text' => 'Expertise dans le maniement d\'objets redondants',
-        'textClass' => 'skillsText',
-    ],
-    ['title' => 'Stratégie',
-        'titleClass' => 'skillsTitle',
-        'iconClass' => 'skillIconCastle',
-        'text' => 'Comptage des briques au centième près de manière péremptoire',
-        'textClass' => 'skillsText',
-    ],
-    ['title' => 'Communication',
-        'titleClass' => 'skillsTitle',
-        'iconClass' => 'skillIconOld',
-        'text' => 'Spécialisé dans la gestion des vieux mystérieux',
-        'textClass' => 'skillsText',
-    ],
-];
-
-foreach ($skills as $skill) {
-    ?><div class="skillContainer"><?php
-    foreach ($skill as $type => $content) {
-
-    }
-
-
-    ?></div><?php
-}
