141ab8045bd401ab4448d9e6316218c5a2f0c556
bloic <loic.benos@gmail.com>
2020-10-08 13:07:43 +0200

    update css and file formation.php

--- Formation.php
+++ Formation.php
@@ -1,33 +1,38 @@
 <section class="formation" id="formation">
     <h2 class="title">Formations</h2>
+    <?php
+
+    $historyDates = ['1400 ans avant JScript',
+                    '1400 ans avant JScript',
+                    '10 ans après JScript',
+                    '1400 ans avant JScript',
+                    ];
+    $dateDiplomas = [
+            'Université de Karadoc' => 'Doctorat de la botte secrète de Karadoc "c\'est pas faux"',
+            'Université Le Gallois' => 'Master de la technique du Rebrousse-Chemin',
+            'Stargate' => 'Testeur de Porte Dimmensionnelles Stargate',
+            'Université Cul de Chouette' => 'Perceval chante le Sloubi',
+        ];
+
+
+    ?>
 
     <div class="containers">
+<?php foreach ($dateDiplomas as $title => $dateDiploma){
+     foreach( $historyDates as $historyDate){ ?>
         <div class="containersDate">
             <div class="date">
-                <div class="historyDate">1400 ans avant JScript</div>
+                <div class="historyDate"><?= $historyDate;};?></div>
             </div>
         </div>
         <div class="row"></div>
 
+
         <div class="cadre">
-            <div class="dateDiploma"><h3>Université de Karadoc</h3>
-                <p>Doctorat de la botte secrète de Karadoc "C'est pas faux"</p></div>
+            <div class="dateDiploma"><h3><?= $title; ?></h3>
+                <p><?= $dateDiploma;};?></p></div>
         </div>
     </div>
 
 
-    <div class="containers">
-        <div class="containersDate">
-            <div class="date">
-                <div class="historyDate">1400 ans avant JScript</div>
-            </div>
-        </div>
-        <div class="row"></div>
-
-        <div class="cadre">
-            <div class="dateDiploma"><h3>Université Le Gallois</h3>
-                <p>Master de La technique du Rebrousse-Chemin</p></div>
-        </div>
-    </div>
 </section>
-<?php
