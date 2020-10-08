6b4254df63c7d12d8fbaab5b05a444053c015074
JulieRebeyrolle <julie.rebeyrolle@gmail.com>
2020-10-08 19:16:18 +0200

    remove uneccesarry comments

--- skills.php
+++ skills.php
@@ -39,23 +39,3 @@
     </div>
 </section>
 
-
-
-
-        <!--<div class="skillContainer">
-            <div class="skillIconSwords"></div>
-            <h3 class="skillsTitle">Combat</h3>
-            <p class="skillsText">Expertise dans le maniement d'objets redondants</p>
-        </div>
-
-        <div class="skillContainer">
-            <div class="skillIconCastle"></div>
-            <h3 class="skillsTitle">Stratégie</h3>
-            <p class="skillsText">Comptage des briques au centième près de manière péremptoire</p>
-        </div>
-
-        <div class="skillContainer">
-            <div class="skillIconOld"></div>
-            <h3 class="skillsTitle">Communication </h3>
-            <p class="skillsText">Spécialisé dans la gestion des vieux mystérieux</p>
-        </div>-->
