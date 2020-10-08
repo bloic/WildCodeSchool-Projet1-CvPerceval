684bbb639483c1020beba7c2ec01f61641eef123
Chenuet Loic <loicchenuet@hotmail.fr>
2020-10-08 14:44:37 +0200

    resolution quote

--- Recommandation.php
+++ Recommandation.php
@@ -9,11 +9,10 @@
             <div><img src="/img/arthur.jpg" alt="Arthur Roi de Bretagne" class="imageArthur"></div>
             <div class="recommandationArthur">
                 <h4>Arthur Pendragon:</h4>
-                <p><img src="/img/left_quote-30_30.png"> Je gueule ; c'est vrai, j'suis un peu sec, tout ça, mais pour quelqu'un comme moi qui a
+                <p>Je gueule ; c'est vrai, j'suis un peu sec, tout ça, mais pour quelqu'un comme moi qui a
                 facilement tendance à la dépression c'est très important ce que vous faites, parce que...
                 Comment vous dire... C'est systématiquement débile mais c'est toujours inattendu.
-                Et ça c'est très important pour la... la santé du... du cigare...
-                <img src="/img/right_quote_30_30.png"></p>
+                Et ça c'est très important pour la... la santé du... du cigare...</p>
             </div>
         </div>
 
@@ -21,10 +20,9 @@
         <div class="karadoc">
             <div class="recommandationKaradoc">
                 <h4>Karadoc de Vannes:</h4>
-                <p><img src="/img/left_quote-30_30.png">Là, y'a les méduses, les insectes. Là, y'a les glandus, les grouillots. Là, y'a les mecs
+                <p>Là, y'a les méduses, les insectes. Là, y'a les glandus, les grouillots. Là, y'a les mecs
                 normaux. Là, y'a les chevaliers. Là, y'a les rois et les princes. Et après, bien au-dessus, y'a le Roi Arthur.
-                Vous, vous aurez eu deux bonhommes dans votre vie, eh ben vous pourrez dire que vous avez tapé dans l'exception.
-                <img src="/img/right_quote_30_30.png"></p>
+                Vous, vous aurez eu deux bonhommes dans votre vie, eh ben vous pourrez dire que vous avez tapé dans l'exception.</p>
             </div>
             <div><img src="/img/Karadoc-150x150.jpg" alt="Karadoc" class="imageKaradoc"></div>
         </div>
@@ -34,9 +32,9 @@
             <div><img src="/img/LeMaîtredArmes-150x150.jpg" alt="Le Maitre d'armes" class="imageMaitreArme"></div>
             <div class="recommandationMaitreArme">
                 <h4>Le Maitre d'armes:</h4>
-                <p><img src="/img/left_quote-30_30.png">Vous savez quoi, Sire ? On va commencer par se faire une saucisse grillée de trois pieds de
+                <p>Vous savez quoi, Sire ? On va commencer par se faire une saucisse grillée de trois pieds de
                 long, avec un tonnelet de pinard chacun, et derrière, peut être bien qu'on se paiera des
-                filles. Ah oui ! A un moment, vive la vie !<img src="/img/right_quote_30_30.png"></p>
+                filles. Ah oui ! A un moment, vive la vie !</p>
             </div>
         </div>
 
@@ -44,9 +42,9 @@
         <div class="angharad">
             <div class="recommandationAngharad">
                 <h4>Angharad:</h4>
-                <p><img src="/img/left_quote-30_30.png">Je dois avouer que la chose a du bon, si c'est pour m'éviter la vue de la grande décarade,
+                <p>Je dois avouer que la chose a du bon, si c'est pour m'éviter la vue de la grande décarade,
                 le défilé des nouvelles têtes, la parade des amoureuses à calcul, les poids lourds de la
-                jambe légère, le festival de la morue, en somme...<img src="/img/right_quote_30_30.png"></p>
+                jambe légère, le festival de la morue, en somme...</p>
             </div>
             <div><img src="/img/Angharad-150x150.jpg" alt="Angharade" class="imageAngharad"></div>
         </div>
@@ -56,8 +54,8 @@
             <div><img src="/img/Léodagan-150x150.jpg" alt="Léodagan" class="imageLeodagan"></div>
             <div class="recommandationLeodagan">
                 <h4>Léodagan de Carmélide:</h4>
-                <p><img src="/img/left_quote-30_30.png">Ooooooooh, non mais faire du labyrinthe avec un trou-de-balle pareil, faut drôlement de la vaillance,
-                hein !<img src="/img/right_quote_30_30.png"></p>
+                <p>Ooooooooh, non mais faire du labyrinthe avec un trou-de-balle pareil, faut drôlement de la vaillance,
+                hein !</p>
             </div>
         </div>
 
